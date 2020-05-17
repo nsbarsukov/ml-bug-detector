5e68a24f31ba9713adabbb00b2ba5629efa9451a
n.barsukov <n.barsukov@macmini-nbarsukov.local>
2020-05-17 15:07:19 +0300

    word2Vec первый рабочий прототип

---
+++ word2Vec/buildWord2VecModel.py
@@ -0,0 +1,41 @@
+import json
+from gensim.models.word2vec import Word2Vec
+from multiprocessing import cpu_count
+
+from shared.helpers.word2VecPreprocessing import filter_tokens_fo_Word2Vec
+
+
+def train_word2Vec_model(path_to_tokenized_scripts_json):
+    tokenized_scripts_json = open(path_to_tokenized_scripts_json)
+    tokenized_scripts_dictionary = json.load(tokenized_scripts_json)
+
+    for fileName in tokenized_scripts_dictionary.keys():
+        tokenized_scripts_dictionary[fileName] = filter_tokens_fo_Word2Vec(tokenized_scripts_dictionary[fileName])
+
+    tokenized_scripts_list_of_lists = list(tokenized_scripts_dictionary.values())
+
+    return Word2Vec(
+        # list of lists. Например: [["cat", "say", "meow"], ["dog", "say", "woof"]]
+        sentences=tokenized_scripts_list_of_lists,
+
+        # The number of dimensions of the embeddings and the default is 100
+        size=300,
+
+        # The maximum distance between a target word and words around the target word. The default window is 5.
+        window=5,
+
+        # финальный размер словаря
+        max_final_vocab=10000,
+
+        # игнорировать слова с частотностью ниже, чем эта (регулируется автоматически, если установлено max_final_vocab)
+        min_count=5,
+
+        workers=cpu_count(),
+
+        # The training algorithm, either CBOW(0) or skip gram(1). The default training algorithm is CBOW
+        sg=0
+    )
+
+
+path_scripts_json = '../data/parsed-scripts.json'
+train_word2Vec_model(path_scripts_json).save('./word2VecModel')
