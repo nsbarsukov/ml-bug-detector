6ddf5f1903649814e19c8df9aea2121bd901758a
n.barsukov <n.barsukov@macmini-nbarsukov.local>
2020-05-27 22:00:38 +0300

    Доработки по детектору багов на основании имен аргументов

--- shared/consts/functionsJsonStoragePropertiesNames.py
+++ shared/consts/functionsJsonStoragePropertiesNames.py
@@ -3,3 +3,5 @@ class FUNCTIONS_JSON_PROPERTIES_NAMES:
     FUNC_NAME = 'functionName'  # под каким ключом в json складывали название функции"
     ARGS_NAMES = 'argumentsNames'  # под каким ключом в json складывали названия аргументов функции
     FUNC_TYPE = 'type'  # под каким ключом в json складывали тип функции
+    FILE_PATH = 'filePath'  # путь к файлу, в котором лежит данная функция
+
