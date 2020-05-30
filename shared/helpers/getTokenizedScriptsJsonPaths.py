957dcca96a45fb760307d149f9f4d52d4e36e0e7
n.barsukov <n.barsukov@macmini-nbarsukov.local>
2020-05-30 22:20:42 +0300

    исправлен баг в обучении word2Vec модели

--- shared/helpers/getTokenizedScriptsJsonPaths.py
+++ shared/helpers/getTokenizedScriptsJsonPaths.py
@@ -2,8 +2,10 @@ from os import walk
 from shared.consts import FOLDER_WITH_JSONS, TOKENIZED_JSON_NAME_PREFIX
 
 
-def get_tokenized_scripts_json_path():
+def get_tokenized_scripts_json_paths():
+    """Получаем список путей к json файлам с токенами (от корня репозитория)"""
     # filenames - Список всех файлов в интересуемой нами директории
     fdirpath, dirnames, filenames = list(walk(FOLDER_WITH_JSONS))[0]
+    tokenJsonsNames = list(filter(lambda script_name: script_name.startswith(TOKENIZED_JSON_NAME_PREFIX), filenames))
 
-    return list(filter(lambda script_name: script_name.startswith(TOKENIZED_JSON_NAME_PREFIX), filenames))
+    return list(map(lambda jsonName: FOLDER_WITH_JSONS + '/' + jsonName, tokenJsonsNames))
